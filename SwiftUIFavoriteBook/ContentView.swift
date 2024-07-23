a594a8a8ebc17d87a0c25086c1f85fb5dd8db7c1
Özcan <ozcanozdemir001@gmail.com>
2024-07-23 15:01:38 +0300

    Initial Commit

---
+++ SwiftUIFavoriteBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIFavoriteBook
+//
+//  Created by Özcan on 23.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIFavoriteBook/SwiftUIFavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFavoriteBookApp.swift
+//  SwiftUIFavoriteBook
+//
+//  Created by Özcan on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
